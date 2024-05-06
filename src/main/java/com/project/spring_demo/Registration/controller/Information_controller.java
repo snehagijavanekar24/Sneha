3008e4ead1488cf43ac1e5775853a99317d4527a
snehagijavanekar24 <166737264+snehagijavanekar24@users.noreply.github.com>
2024-05-06 06:07:25 -0700

    Update Information_controller.java

--- src/main/java/com/project/spring_demo/Registration/controller/Information_controller.java
+++ src/main/java/com/project/spring_demo/Registration/controller/Information_controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/Information")
+@RequestMapping("/Registration")
 public class Information_controller {
     InformationService informationService;
 
